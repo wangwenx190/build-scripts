b295a471c941ca67456b0aab6cbffbc84e230755
wangwenx190 <2546789017@qq.com>
2018-03-17 20:05:49 +0800

    Added build-qt.bat

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# build-scripts
-Build scripts for some open source projects, just for personal use.
+## build-qt.bat
+
+A Windows batch script file to compile Qt from source code. You must know that this batch script can only help you on configuring the compiler, it will not install anything to your system, you still have to install all the prerequisites manually before you run it.
+
+Usage:
+
+CALL script-file-path ARCH(x86 or x64, default is x64) TYPE(dll or lib, default is dll) MODE(debug, release or debug-and-release, default is release) SRC-DIR(Qt source code dir, default is ".\src") INSTALL-DIR(Qt install dir, default is ".\qt") EXTRA-PARAMS(Extra parameters you want to pass to the config program, default is empty)
+
+```bat
+CALL build-qt.bat x64 dll release "C:\Qt\src" "C"\Qt\msvc2017_64"
+```
+
+Tested on:
+
+Windows 10 + MSVC 2017 + Qt 5.11.0
+
+(Theoretically, this batch script supports Qt 5.6 and newer.)
---
+++ build-qt.bat
@@ -0,0 +1,58 @@
+@ECHO OFF
+COLOR
+TITLE Building Qt from source code
+CLS
+SETLOCAL EnableDelayedExpansion
+REM x86 or x64, default is x64
+SET "_TARGET_ARCH=%1"
+REM dll or lib, default is dll
+SET "_BUILD_TYPE=%2"
+REM debug, release or debug-and-release, default is release
+SET "_COMP_MODE=%3"
+REM Qt source code directory, default is ".\src"
+SET "_ROOT=%4"
+REM Qt install directory, default is ".\qt"
+SET "_INSTALL_DIR=%5"
+REM Extra configure parameters, default is empty
+SET "_EXTRA_PARAMS=%6"
+IF /I "%_ROOT%" == "" SET "_ROOT=%~dp0src"
+IF NOT EXIST "%_ROOT%" ECHO Qt source code directory not found. Cannot continue compiling. && GOTO Fin
+IF /I "%_TARGET_ARCH%" == "" SET "_TARGET_ARCH=x64"
+IF /I "%_COMP_MODE%" == "" SET "_COMP_MODE=release"
+IF /I "%_BUILD_TYPE%" == "" SET "_BUILD_TYPE=dll"
+IF /I "%_INSTALL_DIR%" == "" SET "_INSTALL_DIR=%~dp0qt-%_BUILD_TYPE%-%_COMP_MODE%-%_TARGET_ARCH%"
+IF EXIST "%_INSTALL_DIR%" RD /S /Q "%_INSTALL_DIR%"
+MD "%_INSTALL_DIR%"
+IF /I "%_COMP_MODE%" == "debug" (
+    SET "_COMP_MODE=-%_COMP_MODE%"
+) ELSE (
+    SET "_COMP_MODE=-%_COMP_MODE% -strip"
+)
+IF /I "%_BUILD_TYPE%" == "lib" (
+    SET "_BUILD_TYPE=-static -static-runtime -skip qtwebengine"
+) ELSE (
+    SET "_BUILD_TYPE=-shared"
+)
+SET _CFG_PARAMS=-opensource -confirm-license %_COMP_MODE% %_BUILD_TYPE% -platform win32-msvc -ltcg -plugin-manifests -mp -silent -nomake examples -nomake tests -opengl dynamic -prefix "%_INSTALL_DIR%" %_EXTRA_PARAMS%
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" %_TARGET_ARCH%
+SET "PATH=%_ROOT%\qtbase\bin;%_ROOT%\gnuwin32\bin;%PATH%"
+SET "_CFG_BAT=%_ROOT%\configure.bat"
+CD /D "%_ROOT%"
+SET "_ROOT="
+CALL "%_CFG_BAT%" %_CFG_PARAMS%
+REM If you don't have jom, use nmake instead, which is provided by Visual Studio.
+REM nmake is very slow, I recommend you use jom, you can download the latest jom
+REM from it's official download link:
+REM download.qt.io/official_releases/jom/jom.zip
+REM Remember to add it's path to your system path variables
+REM or just put it into "src\gnuwin32\bin", this directory will be added to
+REM the PATH variable temporarily during the compiling stage.
+SET "_JOM=nmake"
+IF EXIST "jom.exe" SET "_JOM=jom"
+%_JOM% && %_JOM% install
+GOTO Fin
+
+:Fin
+ENDLOCAL
+PAUSE
+EXIT /B
