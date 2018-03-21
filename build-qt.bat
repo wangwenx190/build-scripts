e55169bc2e585a380e748144cf66f387c38c7c97
wangwenx190 <2546789017@qq.com>
2018-03-21 23:14:36 +0800

    Update.

--- build-qt.bat
+++ build-qt.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 COLOR
-TITLE Building Qt from source code
+TITLE Configuring Qt
 CLS
 SETLOCAL EnableDelayedExpansion
 REM x86 or x64, default is x64
@@ -41,14 +41,9 @@ IF NOT EXIST "%_VC_CMD%" SET "_VC_CMD=C:\Program Files (x86)\Microsoft Visual St
 IF NOT EXIST "%_VC_CMD%" SET "_VC_CMD=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat"
 IF NOT EXIST "%_VC_CMD%" SET "_VC_CMD=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 IF NOT EXIST "%_VC_CMD%" SET "_VC_CMD=C:\Program Files (x86)\Microsoft Visual Studio\Shared\14.0\VC\vcvarsall.bat"
-IF NOT EXIST "%_VC_CMD%" ECHO Cannot find [vcvarsall.bat], maybe you forget to add VC dir to the PATH variable. && GOTO Fin
+IF NOT EXIST "%_VC_CMD%" ECHO Cannot find [vcvarsall.bat], if you didn't install vs in it's default location, please change this script. && GOTO Fin
 SET _CFG_PARAMS=-opensource -confirm-license %_COMP_MODE% %_BUILD_TYPE% -platform win32-msvc -ltcg -plugin-manifests -mp -silent -nomake examples -nomake tests -opengl dynamic -prefix "%_INSTALL_DIR%" %_EXTRA_PARAMS%
-CALL "%_VC_CMD%" %_TARGET_ARCH%
-SET "PATH=%_ROOT%\qtbase\bin;%_ROOT%\gnuwin32\bin;%PATH%"
 SET "_CFG_BAT=%_ROOT%\configure.bat"
-CD /D "%_ROOT%"
-SET _ROOT=
-CALL "%_CFG_BAT%" %_CFG_PARAMS%
 REM If you don't have jom, use nmake instead, which is provided by Visual Studio.
 REM nmake is very slow, I recommend you use jom, you can download the latest jom
 REM from it's official download link:
@@ -58,7 +53,47 @@ REM or just put it into "src\gnuwin32\bin", this directory will be added to
 REM the PATH variable temporarily during the compiling process.
 SET "_JOM=nmake"
 IF EXIST "jom.exe" SET "_JOM=jom"
-%_JOM% && %_JOM% install
+TITLE Configure finished
+CLS
+ECHO The configuring process have finished successfully
+ECHO Your configuration is:
+ECHO ---------------------------------------
+ECHO Target architecture: %_TARGET_ARCH%
+ECHO Source code directory: %_ROOT%
+ECHO Install directory: %_INSTALL_DIR%
+ECHO vcvarsall.bat: %_VC_CMD%
+ECHO Build tool: %_JOM%
+ECHO Qt configure parameters: %_CFG_PARAMS%
+ECHO ---------------------------------------
+ECHO If everything is all right, press any key to generate the build script
+ECHO If anything is wrong, please close this window and re-run it
+ECHO Your build script will be saved to: %~dp0build.bat
+PAUSE
+SET "_BUILD_BAT=%~dp0build.bat"
+IF EXIST "%_BUILD_BAT%" DEL /F /Q "%_BUILD_BAT%"
+ECHO @ECHO OFF>"%_BUILD_BAT%"
+ECHO COLOR>>"%_BUILD_BAT%"
+ECHO TITLE Building Qt from source code>>"%_BUILD_BAT%"
+ECHO CLS>>"%_BUILD_BAT%"
+ECHO CALL "%_VC_CMD%" %_TARGET_ARCH%>>"%_BUILD_BAT%"
+ECHO SET "_ROOT=%_ROOT%">>"%_BUILD_BAT%"
+ECHO SET "PATH=%%_ROOT%%\qtbase\bin;%%_ROOT%%\gnuwin32\bin;%%PATH%%">>"%_BUILD_BAT%"
+ECHO SET _ROOT=>>"%_BUILD_BAT%"
+ECHO CD /D "%_ROOT%">>"%_BUILD_BAT%"
+ECHO CALL "%_CFG_BAT%" %_CFG_PARAMS%>>"%_BUILD_BAT%"
+ECHO %_JOM%>>"%_BUILD_BAT%"
+ECHO %_JOM% install>>"%_BUILD_BAT%"
+ECHO TITLE Finished>>"%_BUILD_BAT%"
+ECHO ECHO Compiling process have finished successfully>>"%_BUILD_BAT%"
+ECHO ECHO All binaries have been installed to: %_INSTALL_DIR%>>"%_BUILD_BAT%"
+ECHO ECHO Press any key to exit this program>>"%_BUILD_BAT%"
+ECHO PAUSE>>"%_BUILD_BAT%"
+ECHO EXIT /B>>"%_BUILD_BAT%"
+CLS
+ECHO Build script have been saved to: %_BUILD_BAT%
+ECHO Press any key to run it, or you can close this window and run it manually
+PAUSE
+CALL "%_BUILD_BAT%"
 GOTO Fin
 
 :Fin
